2450bfae8b9263d0315694f483ac2cf3d9d1915e
James Leahy <defuncart@gmail.com>
2021-12-15 17:18:53 +0100

    Add ChatRepository.

---
+++ lib/infrastructure/chat/chat_repository_impl.dart
@@ -0,0 +1,83 @@
+import 'dart:developer' show log;
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:messenger/common/repositories/date_time/date_time_generator.dart';
+import 'package:messenger/common/repositories/id/id_generator.dart';
+import 'package:messenger/domain/chat/chat_repository.dart';
+import 'package:messenger/domain/chat/entity/chat_entity.dart';
+import 'package:messenger/infrastructure/chat/chat.mapper.dart';
+import 'package:messenger/infrastructure/chat/dto/chat_dto.dart';
+
+class ChatRepositoryImpl implements ChatRepository {
+  ChatRepositoryImpl({
+    FirebaseFirestore? firebaseFirestore,
+    required DateTimeGenerator dateTimeGenerator,
+    required IdGenerator idGenerator,
+  })  : _firebaseFirestore = firebaseFirestore ?? FirebaseFirestore.instance,
+        _dateTimeGenerator = dateTimeGenerator,
+        _idGenerator = idGenerator;
+
+  final FirebaseFirestore _firebaseFirestore;
+  final DateTimeGenerator _dateTimeGenerator;
+  final IdGenerator _idGenerator;
+
+  static const _collection = 'chats';
+
+  /// Saves a chat to db
+  ///
+  /// If the chat does not exist, it is created, otherwise existing chat overwritten
+  Future<void> _saveChat(ChatDto chat) async {
+    await _firebaseFirestore.collection(_collection).doc(chat.id).set(chat.toJson());
+    log('$ChatRepositoryImpl saved chat ${chat.toString()}');
+  }
+
+  /// Maps [ChatDto] to [ChatEntity]
+  ChatEntity _dtoToEntity(ChatDto dto) => ChatEntity.value(dto.toValueObject());
+
+  /// Maps [DocumentSnapshot] to [ChatEntity]
+  ChatEntity _snapshotToEntity(DocumentSnapshot<Map<String, dynamic>> snapshot) {
+    if (snapshot.exists) {
+      try {
+        final dto = ChatDto.fromJson(snapshot.data()!);
+        return _dtoToEntity(dto);
+      } catch (e) {
+        return ChatEntity.failure(ChatCouldNotBeParsedFailure());
+      }
+    }
+    return ChatEntity.failure(ChatNotFoundFailure());
+  }
+
+  @override
+  Future<ChatEntity> createChat({
+    required List<String> userIds,
+    required String createdBy,
+  }) async {
+    final id = _idGenerator.generate();
+    final now = _dateTimeGenerator.nowUtc;
+    final message = ChatDto(
+      id: id,
+      userIds: userIds,
+      createdBy: createdBy,
+      createdAt: now,
+    );
+
+    await _saveChat(message);
+
+    return _dtoToEntity(message);
+  }
+
+  @override
+  Future<ChatEntity> getChat({required String id}) async {
+    final snapshot = await _firebaseFirestore.collection(_collection).doc(id).get();
+    return _snapshotToEntity(snapshot);
+  }
+
+  @override
+  Future<void> deleteChat({required String id}) async {
+    final now = _dateTimeGenerator.nowUtc.toIso8601String();
+    await _firebaseFirestore.collection(_collection).doc(id).update({
+      'deletedAt': now,
+    });
+    log('$ChatRepositoryImpl delete chat with $id');
+  }
+}
---
+++ test/infrastructure/chat/chat_repository_impl_test.dart
@@ -0,0 +1,156 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:messenger/domain/chat/entity/chat_entity.dart';
+import 'package:messenger/infrastructure/chat/chat_repository_impl.dart';
+import 'package:mocktail/mocktail.dart';
+
+import '../../mocks.dart';
+import '../../test_utils.dart';
+
+void main() {
+  group('$ChatRepositoryImpl', () {
+    late FirebaseFirestore fakeFirebaseFirestore;
+    late MockDateTimeGenerator mockDateTimeGenerator;
+    late MockIdGenerator mockIdGenerator;
+    late ChatRepositoryImpl chatRepositoryImpl;
+
+    setUp(() {
+      fakeFirebaseFirestore = FakeFirebaseFirestore();
+      mockDateTimeGenerator = MockDateTimeGenerator();
+      mockIdGenerator = MockIdGenerator();
+      chatRepositoryImpl = ChatRepositoryImpl(
+        firebaseFirestore: fakeFirebaseFirestore,
+        dateTimeGenerator: mockDateTimeGenerator,
+        idGenerator: mockIdGenerator,
+      );
+    });
+
+    test('createChat', () async {
+      final now = DateTime(1);
+      when(() => mockDateTimeGenerator.nowUtc).thenReturn(now);
+      const id = 'id';
+      when(() => mockIdGenerator.generate()).thenReturn(id);
+
+      const title = '';
+      const userIds = ['user1'];
+      const createdBy = 'createdBy';
+
+      final chat = await chatRepositoryImpl.createChat(
+        userIds: userIds,
+        createdBy: createdBy,
+      );
+
+      expect(
+        chat,
+        TestEntities.chat(
+          id: id,
+          title: title,
+          userIds: userIds,
+          createdBy: createdBy,
+        ),
+      );
+
+      final snapshot = await fakeFirebaseFirestore.collection('chats').doc(id).get();
+
+      expect(
+        snapshot.data(),
+        {
+          'id': 'id',
+          'messages': [],
+          'title': '',
+          'userIds': ['user1'],
+          'createdBy': 'createdBy',
+          'createdAt': '0001-01-01T00:00:00.000',
+          'updatedAt': null,
+          'deletedAt': null,
+        },
+      );
+    });
+
+    group('getChat', () {
+      const id = 'id';
+
+      test('when no chat in db, expect $ChatNotFoundFailure failure', () async {
+        final chat = await chatRepositoryImpl.getChat(id: id);
+        expect(chat, ChatEntity.failure(ChatNotFoundFailure()));
+      });
+
+      test('when chat data in db is not valid, expect $ChatCouldNotBeParsedFailure failure', () async {
+        await fakeFirebaseFirestore.collection('chats').doc(id).set({});
+        final chat = await chatRepositoryImpl.getChat(id: id);
+        expect(chat, ChatEntity.failure(ChatCouldNotBeParsedFailure()));
+      });
+
+      test('when chat in db, expect entity', () async {
+        await fakeFirebaseFirestore.collection('chats').doc(id).set({
+          'id': 'id',
+          'messages': [],
+          'title': '',
+          'userIds': ['user1'],
+          'createdBy': 'createdBy',
+          'createdAt': '0001-01-01T00:00:00.000',
+          'updatedAt': null,
+          'deletedAt': null,
+        });
+
+        final chat = await chatRepositoryImpl.getChat(id: id);
+        expect(chat, TestEntities.chat(userIds: ['user1']));
+      });
+    });
+
+    group('deleteChat', () {
+      const id = 'id';
+
+      test('when chat in db, expect deletedAt is not null', () async {
+        final now = DateTime(1);
+        when(() => mockDateTimeGenerator.nowUtc).thenReturn(now);
+
+        await fakeFirebaseFirestore.collection('chats').doc(id).set({
+          'id': 'id',
+          'messages': [],
+          'title': '',
+          'userIds': ['user1'],
+          'createdBy': 'createdBy',
+          'createdAt': '0001-01-01T00:00:00.000',
+          'updatedAt': null,
+          'deletedAt': null,
+        });
+
+        await chatRepositoryImpl.deleteChat(id: id);
+
+        final snapshot = await fakeFirebaseFirestore.collection('chats').doc(id).get();
+        expect(
+          snapshot.data(),
+          {
+            'id': 'id',
+            'messages': [],
+            'title': '',
+            'userIds': ['user1'],
+            'createdBy': 'createdBy',
+            'createdAt': '0001-01-01T00:00:00.000',
+            'updatedAt': null,
+            'deletedAt': '0001-01-01T00:00:00.000',
+          },
+        );
+      });
+    });
+  });
+
+  test('When firebase is not faked, expect that FirebaseFirestore is default', () async {
+    final mockDateTimeGenerator = MockDateTimeGenerator();
+    final mockIdGenerator = MockIdGenerator();
+
+    MethodChannelMocks.setupFirebase();
+    await Firebase.initializeApp();
+
+    expect(
+      () => ChatRepositoryImpl(
+        dateTimeGenerator: mockDateTimeGenerator,
+        idGenerator: mockIdGenerator,
+      ),
+      returnsNormally,
+    );
+  });
+}
